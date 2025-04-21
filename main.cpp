3fbe9b15f3fb25e0623d38554c052007ece4bafa
meedoomostafa <mezo.225577@gmail.com>
2025-04-21 16:44:20 -0400

    Filteration Added Successfuly

--- main.cpp
+++ main.cpp
@@ -1,42 +1,50 @@
 #include "lesser_pandas.h"
 
 int main() {
-    DataFrame df("data.csv");
+    try {
+        DataFrame df("titanic.csv");
 
-    cout << "Full DataFrame:\n";
-    cout << df << endl;
+        // cout << "Full DataFrame:\n";
+        // cout << df << endl;
 
-    cout << "First 3 rows:\n";
-    df.head(3);
+        // cout << "First 3 rows:\n";
+        // df.head(3);
 
-    cout << "Last 2 rows:\n";
-    df.tail(2);
+        // cout << "Last 2 rows:\n";
+        // df.tail(2);
 
-    // Access a single column
-    Column& age_col = df["Age"];
-    cout << "Age column:\n" << age_col << endl;
+        // // Access a single column
+        // Column& age_col = df["Age"];
+        // cout << "Age column:\n" << age_col << endl;
 
-    // Compute mean of Age column (ignoring missing values)
-    cout << "Mean age: " << age_col.mean() << endl;
+        // // Compute mean of Age column (ignoring missing values)
+        // cout << "Mean age: " << age_col.mean() << endl;
 
-    age_col.fillna(age_col.mean());
-    cout << "Age column after filling missing values with the average:\n" << age_col << endl;
-    
-    // Save the DataFrame to a file named "output.csv"
-    df.save_to_csv("output.csv", true, "|", true, "N/A", {"Age", "Salary"});
+        // age_col.fillna(age_col.mean());
+        // cout << "Age column after filling missing values with the average:\n" << age_col << endl;
 
-    df.fillna(5);
-    cout << "DataFrame after filling missing values:\n";
-    cout << df << endl;
+        // // Save the DataFrame to a file named "output.csv"
+        // df.save_to_csv("output.csv", true, "|", true, "N/A", {"Age", "Salary"});
 
-    // Rename columns
-    df.rename({{"Age", "Years"}, {"Salary", "Income"}});
-    cout << "DataFrame after renaming columns:\n";
-    cout << df << endl;
-    cout << df["Income"];
+        // df.fillna(5);
+        // cout << "DataFrame after filling missing values:\n";
+        // cout << df << endl;
 
-    cout << "Min Salary: " << df["Income"].min() << endl;
-    cout << "Max Salary: " << df["Income"].max() << endl;
+        // // Rename columns
+        // df.rename({{"Age", "Years"}, {"Salary", "Income"}});
+        // cout << "DataFrame after renaming columns:\n";
+        // cout << df << endl;
+        // cout << df["Income"];
 
+        // cout << "Min Salary: " << df["Income"].min() << endl;
+        // cout << "Max Salary: " << df["Income"].max() << endl;
+
+        auto newData = df[df["PassengerId"] > 600];
+        cout << newData << endl;
+
+    } catch (const exception& e) {
+        cerr << "Error: " << e.what() << std::endl;
+        return 1;
+    }
     return 0;
 }
